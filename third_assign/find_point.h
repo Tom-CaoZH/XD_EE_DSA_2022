4b4dc3446b30a46b9a76a4e31660b3e9a2f35f23
Tom-CaoZH <tomcaoTTT@outlook.com>
2022-10-26 03:17:28 +0000

    finish third assign

--- third_assign/find_point.h
+++ third_assign/find_point.h
@@ -1,3 +1,37 @@
+// return the index of the min number(begin from 0)
+int find_min(int* elem) {
+    int min_index = 0;
+    for(int i = 0;i < n; ++i) {
+        if(elem[i] < elem[min_index]) {
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+// check the elem[row, col] is the max number in the col
+bool check_max_in_col(array* data, int elem, int col) {
+    for(int i = 0;i < m; ++i) {
+        if(data->A[i][col] > elem) {
+            return false;
+        }
+    }
+    return true;
+}
+
 void minmax(array* data) {
-    
+    int count = 0;
+    for(int i = 0;i < m; ++i) {
+        //find the min elem in every row
+        int min_index_in_row = find_min(data->A[i]);
+        if(check_max_in_col(data, data->A[i][min_index_in_row], min_index_in_row)) {
+            count++;
+            std::cout << "Find!" << std::endl;
+            std::cout << "row = " << i+1 << " " << "col = " << min_index_in_row+1 << " " 
+                        << "elem = " << data->A[i][min_index_in_row] << std::endl;
+        }
+    }
+    if(count == 0) {
+        std::cout << "There is no target point!" << std::endl;
+    }
 }
--- third_assign/main.cpp
+++ third_assign/main.cpp
@@ -4,12 +4,23 @@
 #include "find_point.h"
 using namespace std;
 
+void print_all_matrics(array* data) {
+  std::cout <<"The input array:" << std::endl;
+  for(int i = 0;i <m; ++i) {
+    for(int j = 0;j < n; ++j) {
+      std::cout << data->A[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
 int main(void) {
     array* pa=new array;
     int i, j;
     for (i=0;i<m;i++)
       for (j=0;j<n;j++)
         cin>>pa->A[i][j];
+    print_all_matrics(pa);
     minmax(pa);
 }
 
